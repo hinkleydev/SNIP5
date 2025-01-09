d08a253dbd3618239f75db7826b9551e06ba111e
hinkleydev <azz@hinkley.dev>
2025-01-09 15:58:04 +0000

    ADD: Language readout

--- flaskr/api/v1.py
+++ flaskr/api/v1.py
@@ -3,6 +3,12 @@ from api.reader import data
 
 api = Blueprint('api', __name__)
 
+# Root - Returns amount of snippets and languages in use
 @api.route('/')
 def root():
-    return jsonify({"snippets" : len(data)})
+    languages = []
+    for each in data :
+        languages.append(each["lang"])
+    languages = set(languages)
+    languages = list(languages)
+    return jsonify({"snippets" : len(data), "languages" : languages})
