54e6f6c58c1ef8730952e05efb81c56c966fc50c
hinkleydev <azz@hinkley.dev>
2025-01-09 16:22:23 +0000

    ADD: Specific snippet handler

--- flaskr/api/v1.py
+++ flaskr/api/v1.py
@@ -4,11 +4,19 @@ from api.reader import data
 api = Blueprint('api', __name__)
 
 # Root - Returns amount of snippets and languages in use
-@api.route('/')
+@api.route('/snippets')
 def root():
     languages = []
     for each in data :
         languages.append(each["lang"])
-    languages = set(languages)
-    languages = list(languages)
+    languages = set(languages) # Remove the duplicates
+    languages = list(languages) # Remake list to add to JSON
     return jsonify({"snippets" : len(data), "languages" : languages})
+
+# Specific - Returns all the details of a specific snippet
+@api.route('/snippets/<int:id>')
+def byID(id):
+    for each in data :
+        if id != each["id"] : continue # Pass if not what we're after
+        return jsonify(each)
+    return jsonify({"error" : "not found"}), 404 # Default if nothing is found
