f39af206256785ee9aa993a8a3077f17a7ef732f
hinkleydev <azz@hinkley.dev>
2025-01-09 15:22:58 +0000

    DEL: __init__.py cause renamed to main.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template, jsonify
-
-snippets = Blueprint('simple_page', __name__)
-
-@snippets.route('/', defaults={'page': 'index'})
-def all(page):
-    return jsonify({"message": "working"})
