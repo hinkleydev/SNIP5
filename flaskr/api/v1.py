21ea1282b3959f489d64c67ee01040d23e372537
hinkleydev <azz@hinkley.dev>
2025-01-10 09:31:55 +0000

    ADD: Snippet creation function

--- flaskr/api/v1.py
+++ flaskr/api/v1.py
@@ -1,10 +1,33 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from api.reader import data
 
 api = Blueprint('api', __name__)
 
+# Create - Add snippet to the list
+@api.post('/snippets')
+def add():
+    idList = []
+    for each in data :
+        idList.append(each["id"])
+    idList.sort()
+    nextId = idList[-1] + 1; # Get the next ID by organising the IDs and adding 1 to the biggest
+    userInput = request.json # This rejects it if it isn't JSON
+    if userInput.get("lang") == None :
+        return jsonify({"error" : "lang not specified"})
+    if userInput.get("data") == None :
+        return jsonify({"error" : "data not specified"})
+    # Check the object has the required fields and tell the user if not
+    newObject = {
+        "id" : nextId,
+        "lang" : userInput["lang"],
+        "data" : userInput["data"]
+    }
+    data.append(newObject)
+    # Make new object and add to array
+    return jsonify(newObject), 201
+
 # Root - Returns amount of snippets and languages in use
-@api.route('/snippets')
+@api.get('/snippets')
 def root():
     languages = []
     for each in data :
