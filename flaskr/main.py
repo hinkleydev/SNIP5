47050670fe88836c92c7f502bc49cb98cbc9acdb
hinkleydev <azz@hinkley.dev>
2025-01-22 10:25:41 +0000

    ADD: Login function and token checking on API

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,15 +1,22 @@
 from flask import Flask, request, abort, jsonify
-from api import v1
-import bcrypt, jwt
+from api.v1 import api
+import bcrypt, jwt, os
 from cryptography.fernet import Fernet
 
 users = []
 SALT = bcrypt.gensalt()
-JWT_TOKEN = Fernet.generate_key()
+JWT_TOKEN = os.environ["JWT_TOKEN"]
+
+@api.before_request
+def loggedIn() :
+    try :
+        jwt.decode(request.headers.get("Authorization", ""), JWT_TOKEN, algorithms=["HS256"])
+        # This doesn't compare to the user, if they have a valid JWT, it can only have been issued by the server cause of the token
+    except :
+        abort(403, "Invalid authorization token recieved")
 
 app = Flask(__name__)
-
-app.register_blueprint(v1.api, url_prefix = "/api/v1")
+app.register_blueprint(api, url_prefix = "/api/v1")
 
 @app.post("/register")
 def register() :
@@ -27,3 +34,20 @@ def register() :
     encoded = jwt.encode(userObject, JWT_TOKEN, algorithm="HS256")
     return jsonify({"status" : "success", "token" : encoded})
 
+@app.post("/login")
+def login() :
+    data = request.json
+    username = data["username"]
+    password = data["password"]
+    passwordBytes = bytes(password, encoding="utf-8")
+    loggedInUser = None
+    for user in users : 
+        if user["username"] == username : # Found user
+            hash = user["password"]
+            hashBytes = bytes(hash, encoding="utf-8")
+            if bcrypt.checkpw(passwordBytes, hashBytes): # Correct password
+                loggedInUser = user
+    if not loggedInUser :
+        abort(401, "Incorrect login")
+    encoded = jwt.encode(loggedInUser, JWT_TOKEN, algorithm="HS256")
+    return jsonify({"status" : "success", "token" : encoded})
